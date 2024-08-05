a711dcbab4761a09e4a3fe83cd69883048c4abed
Rohit Alamgari <rohit.alamgari@gmail.com>
2024-08-05 17:34:24 -0500

    version of the website I like

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import fasthtml.common as fh
+from markdown import markdown
+import fh_bootstrap as bs
 
 app, rt = fh.fast_app()
+
+#from fasthtml's page
+md_exts='codehilite', 'smarty', 'extra', 'sane_lists'
+def Markdown(s, exts=md_exts, **kw): return bs.Div(bs.NotStr(markdown(s, extensions=exts)), **kw)
+
+
+main_section_txt = """
+### Hi there!
+My name is [Rohit Alamgari](https://www.linkedin.com/in/rohit-alamgari/)
+
+I'm currently a junior at [UT studying CS](https://www.cs.utexas.edu/) who's interested in ML, Computer Vision, NLP, and Cloud Computing
+
+
+Here's a link to my [Github](https://github.com/rohitalamgari) and my [Resume](https://drive.google.com/file/d/166uf9n7F6tm0T6XLSdyenrphcf7rUq1u/view?usp=sharing)
+
+Thanks for visiting!
+"""
 
 @rt("/")
 def get():
     return (
-        fh.Title('Rohit Alamgari'),
-        fh.Header('Hello! My name is Rohit Alamgari'),        
-        fh.P(fh.A('Learn more about me', href='/about-me')),
-        fh.P(fh.A('See my professional experience', href='/experience')),
-        fh.P(fh.A('See what I\'ve learnt', href='/education')),
-    )
-
-@rt('/about-me')
-def get():   
-    return(
-        fh.P('WIP'),
-        fh.P(fh.A('Return to home page', href='/'))
-    )
-@rt('/experience')
-def get():
-    return(
-        fh.P('WIP'),
-        fh.P(fh.A('Return to home page', href='/'))
-    )
-@rt('/education')
-def get():
-    return(
-        fh.P('WIP'),
-        fh.P(fh.A('Return to home page', href='/'))
+        fh.Div(Markdown(main_section_txt))
     )
 
 fh.serve()

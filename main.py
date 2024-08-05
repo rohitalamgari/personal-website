c503b7cc57eaceec9097b368f8ef5b17d81866ec
Rohit Alamgari <rohit.alamgari@gmail.com>
2024-08-05 16:05:35 -0500

    testing if vercel allows for domain explansion (not JJK)

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ import fasthtml.common as fh
 
 app, rt = fh.fast_app()
 
-
 @rt("/")
 def get():
     return (
-        fh.Titled('Welcome!',
-                  fh.P(
-                      'My name is Rohit Alamgari'
-                  ))
+        fh.Title('Rohit Alamgari'),
+        fh.Header('Hello! My name is Rohit Alamgari'),        
+        fh.P(fh.A('Learn more about me', href='/about-me')),
+        fh.P(fh.A('See my professional experience', href='/experience')),
+        fh.P(fh.A('See what I\'ve learnt', href='/education')),
     )
 
+@rt('/about-me')
+def get():   
+    return(
+        fh.P('WIP'),
+        fh.P(fh.A('Return to home page', href='/'))
+    )
+@rt('/experience')
+def get():
+    return(
+        fh.P('WIP'),
+        fh.P(fh.A('Return to home page', href='/'))
+    )
+@rt('/education')
+def get():
+    return(
+        fh.P('WIP'),
+        fh.P(fh.A('Return to home page', href='/'))
+    )
 
 fh.serve()
